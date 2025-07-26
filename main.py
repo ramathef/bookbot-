d1f3fb7f2931980cea54d80591fade5bd1a12148
ramathef <ramaganteng1011@gmail.com>
2025-07-26 13:21:02 +0700

    C: Print Report Task

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from stats import *
 
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_txt(book_path)
+    num_word = word_count(text)
+    count_char = char_count(text)
+    list_of_dict = shorted_dict(count_char)
+    print_report(book_path, num_word, list_of_dict)
+
+def print_report (book_path, num_word, list_of_dict):
+    print(f"============= BOOKBOT =============")
+    print(f"Analyzing book found at {book_path}...")
+    print(f"------------ Word Count ------------")
+    print(f"Found {num_word} total words")
+    print(f"--------- Character Count ---------")
+    for item in list_of_dict:
+        if not item["char"].isalpha():
+            continue
+        else:
+            print(f"{item["char"]}: {item["num"]}")
+
+
 def get_book_txt(path_to_file):
     with open(path_to_file) as f:
         return f.read()
-
-def main():
-    text = get_book_txt("books/frankenstein.txt")
-    num_word = word_count(text)
-    count_char = char_count(text)
-    print(count_char)
-    print(f"{num_word} words found in the document")
-    
 
 main()
 
--- stats.py
+++ stats.py
@@ -3,15 +3,30 @@ def word_count(text):
     return len(word)
 
 def char_count(text):
-    char_set = {}
+    char_dict= {}
     lowercase_text = text.lower()
     for char in lowercase_text:
-        if char in char_set:
-            char_set[char] += 1 
+        if char in char_dict:
+            char_dict[char] += 1 
         else:
-            char_set[char] = 1
-            
-    return char_set
+            char_dict[char] = 1   
+    return char_dict
 
+def sort_on(d):
+    return d["num"]
+
+def shorted_dict(char_count):
+    sorted_list_of_dict = []
+
+    for char in char_count:
+        sorted_list_of_dict.append({
+            "char": char,
+            "num": char_count[char]
+        })
+
+    # Sort the list by "num", descending
+    sorted_list_of_dict.sort (reverse=True, key=sort_on)
+
+    return sorted_list_of_dict
 
 
